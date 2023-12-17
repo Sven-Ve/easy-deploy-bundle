cd51fd19bbfd1df6f94a653f47a975e4dafcba10
Sven Vetter <git.09o@svenvetter.com>
2023-12-17 17:11:33 +0100

    ready for symfony 6.4 and 7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,8 @@
 ## Version v1.1.0
 *Thu, 28 Apr 2022 13:15:01 +0000*
 - initial release
+
+
+## Version v1.2.0
+*Sun, 17 Dec 2023 16:11:33 +0000*
+- ready for symfony 6.4 and 7
--- bin/release.php
+++ bin/release.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php
 
-$version = "v1.1.0";
-$message = "initial release";
+$version = "v1.2.0";
+$message = "ready for symfony 6.4 and 7";
 
 /* echo("Running phpstan:\n");
 system("composer run-script phpstan", $res);
